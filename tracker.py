7eb5a1d7f8a85b4f1faed7f8a2ffb2732a21c648
Razerkatt <frogdogp@gmail.com>
2026-07-22 23:52:39 -0500

    tracker.py

--- tracker.py
+++ tracker.py
@@ -4,16 +4,14 @@ import requests
 
 API_KEY = os.environ["HENRIK_API_KEY"]
 
-USERNAME = "Razerkatt"
-TAG = "0101"
-REGION = "na"
+PUUID = "223f6f3c-d8e6-53fc-9e02-394e87e51883"
 
 headers = {
     "Authorization": API_KEY
 }
 
 response = requests.get(
-    f"https://api.henrikdev.xyz/valorant/v1/account/{USERNAME}/{TAG}",
+    f"https://api.henrikdev.xyz/valorant/v3/by-puuid/matches/na/{PUUID}",
     headers=headers
 )
 
